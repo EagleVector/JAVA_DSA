3596d03ab011a8d94a75f7249d3e0cfbaf4458a9
EagleVector <kumarsubhambili3@gmail.com>
2023-02-03 16:30:00 +0530

    Order Agnostic

--- src/com/dsa/BinarySearch.java
+++ src/com/dsa/BinarySearch.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BinarySearch {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] arr = {12, 32, 35, 78, 87, 90, 93, 98, 105, 567};
+        int[] arr = {342, 123, 112, 109, 89, 78, 48, 38, 27, 19, 14, 12, 3};
         System.out.print("Enter the target: ");
         int target = input.nextInt();
         int ans = Binarysrch(arr, target);
@@ -18,18 +18,60 @@ public class BinarySearch {
         int start = 0;
         int end = arr.length - 1;
 
-        while(start <= end) {
-            int mid = start + (end - start) / 2;
+        if (arr[start] < arr[end]) {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
 
-            if (target > arr[mid]) {
-                start = mid + 1;
-            }else if (target < arr[mid]) {
-                end = mid - 1;
+                if (target > arr[mid]) {
+                    start = mid + 1;
+                } else if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    return mid;
+                }
             }
-            else {
-                return mid;
+        } else if (arr[start] > arr[end]) {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+
+                if (target < arr[mid]) {
+                    start = mid + 1;
+                } else if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    return mid;
+                }
             }
         }
         return -1;
     }
 }
+
+
+//        while(start <= end) {
+//            int mid = start + (end - start) / 2;
+//
+//            if (target > arr[mid]) {
+//                start = mid + 1;
+//            }else if (target < arr[mid]) {
+//                end = mid - 1;
+//            }
+//            else {
+//                return mid;
+//            }
+//        }
+//        while(start <= end) {
+//            int mid = start + (end - start) / 2;
+//
+//            if (target < arr[mid]) {
+//                start = mid + 1;
+//            }else if (target > arr[mid]) {
+//                end = mid - 1;
+//            }
+//            else {
+//                return mid;
+//            }
+//        }
+//        return -1;
+//    }
+//}
