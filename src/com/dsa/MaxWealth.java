bc86ed757d12ba1c113e1d43b0837b47600faadd
EagleVector <kumarsubhambili3@gmail.com>
2023-02-03 11:16:31 +0530

    modified sum bank balance

--- src/com/dsa/MaxWealth.java
+++ src/com/dsa/MaxWealth.java
@@ -3,8 +3,11 @@ package com.dsa;
 public class MaxWealth {
     public static void main(String[] args) {
         int[][] arr2D = {
-                {1,2,3,78},
-                {3,2,1,9}
+                {1, 2, 3},
+                {3, 2, 1, 9, 8},
+                {5, 6},
+                {5},
+                {0}
         };
 
         int ans = richer(arr2D);
@@ -13,23 +16,16 @@ public class MaxWealth {
 
     // Brute-Force Technique. Very general idea.
     static int richer(int[][] arr2D) {
-        int wealth1 = 0;
-        int wealth2 = 0;
-        for (int i = 0; i < 1; i++) {
+        int sum = 0;
+        for (int i = 0; i < arr2D.length; i++) {
+            int total = 0;
             for (int j = 0; j < arr2D[i].length; j++) {
-                wealth1 = wealth1 + arr2D[i][j];
+                total = total + arr2D[i][j];
+            }
+            if (total > sum) {
+                sum = total;
             }
         }
-        for (int i = 1; i < 2; i++) {
-            for (int j = 0; j < arr2D[i].length; j++) {
-                wealth2 = wealth2 + arr2D[i][j];
-            }
-        }
-
-        if (wealth1 > wealth2) {
-            return wealth1;
-        }
-
-        return wealth2;
+        return sum;
     }
 }
