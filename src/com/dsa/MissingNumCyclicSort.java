edfea4a8a61adf2d42c5886910f5cb6e47c61a77
EagleVector <kumarsubhambili3@gmail.com>
2023-02-09 19:30:44 +0530

    multiple missing nums

--- src/com/dsa/MissingNumCyclicSort.java
+++ src/com/dsa/MissingNumCyclicSort.java
@@ -1,14 +1,17 @@
 package com.dsa;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MissingNumCyclicSort {
     public static void main(String[] args) {
         int[] arr = {4, 3, 2, 7, 8, 2, 3, 1};
-        missingnumbers(arr);
+        List<Integer> answer = missingnumbers(arr);
+        System.out.println(answer);
     }
 
-    static void missingnumbers(int[] arr) {
+    static List<Integer> missingnumbers(int[] arr) {
 //        int i = 0;
 //        while (i < arr.length) {
 //            int index = arr[i];
@@ -33,11 +36,14 @@ public class MissingNumCyclicSort {
                 i++;
             }
         }
+
+        List<Integer> ans = new ArrayList<>();
         for (int j = 0; j < arr.length; j++) {
             if (arr[j] != j + 1) {
-                System.out.println(j);
+                ans.add(j + 1);
             }
         }
+        return ans;
     }
 
     static void swapix(int[] arr, int first, int second) {
