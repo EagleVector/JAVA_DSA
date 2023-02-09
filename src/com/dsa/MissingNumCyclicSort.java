bb5265222ad37dc00debefd6d232bec945e634c7
EagleVector <kumarsubhambili3@gmail.com>
2023-02-09 18:50:27 +0530

    Multiple Missing Numbers Search

--- src/com/dsa/MissingNumCyclicSort.java
+++ src/com/dsa/MissingNumCyclicSort.java
@@ -1,28 +1,43 @@
 package com.dsa;
 
+import java.util.Arrays;
+
 public class MissingNumCyclicSort {
     public static void main(String[] args) {
-        int[] arr = {4, 0, 2, 1};
-        int ans = missingnumber(arr);
-        System.out.println(ans);
+        int[] arr = {4, 3, 2, 7, 8, 2, 3, 1};
+        missingnumbers(arr);
     }
 
-    static int missingnumber(int[] arr) {
+    static void missingnumbers(int[] arr) {
+//        int i = 0;
+//        while (i < arr.length) {
+//            int index = arr[i];
+//            if (arr[i] < arr.length && arr[i] != arr[index]){
+//                swapix(arr, i, index);
+//            } else {
+//                i++;
+//            }
+//        }
+//        for (int j = 0; j < arr.length; j++) {
+//            if (arr[j] != j) {
+//                return j;
+//            }
+//        }
+//        return arr.length;
         int i = 0;
         while (i < arr.length) {
-            int index = arr[i];
-            if (arr[i] < arr.length && arr[i] != arr[index]){
+            int index = arr[i] - 1;
+            if (arr[i] != arr[index]) {
                 swapix(arr, i, index);
             } else {
                 i++;
             }
         }
         for (int j = 0; j < arr.length; j++) {
-            if (arr[j] != j) {
-                return j;
+            if (arr[j] != j + 1) {
+                System.out.println(j);
             }
         }
-        return arr.length;
     }
 
     static void swapix(int[] arr, int first, int second) {
