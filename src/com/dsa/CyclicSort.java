408ce4d61b38893070029f9883e2755304f2be98
EagleVector <kumarsubhambili3@gmail.com>
2023-02-09 18:16:57 +0530

    Cyclic Sort

--- src/com/dsa/CyclicSort.java
+++ src/com/dsa/CyclicSort.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 public class CyclicSort {
     // Whenever the question defines range 1 to N ---> Use Cyclic Sort.
+    // Space Complexity: O(1) No auxillary extra space required.
+    // Time Complexity: O(N)
     public static void main(String[] args) {
-        int[] arr = {3, 5, 2, 1, 4};
-        int[] ans = cycle(arr);
-        System.out.println(Arrays.toString(ans));
+        int[] arr = {1, 2, 3, 5, 4};
+        cycle(arr);
+        System.out.println(Arrays.toString(arr));
     }
-    static int[] cycle(int[] arr) {
+    static void cycle(int[] arr) {
         int i = 0;
         while(i < arr.length) {
             int index = arr[i] - 1;
@@ -19,7 +21,6 @@ public class CyclicSort {
                 i++;
             }
         }
-        return arr;
     }
 
     static int[] swapindx(int[] arr, int first, int second) {
---
+++ src/com/dsa/MissingNumCyclicSort.java
@@ -0,0 +1,37 @@
+package com.dsa;
+
+import java.util.Arrays;
+
+
+
+public class MissingNumCyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 0, 2, 1};
+        int ans = missingnumber(arr);
+        System.out.println(ans);
+    }
+
+    static int missingnumber(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int index = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[index]){
+                swapix(arr, i, index);
+            } else {
+                i++;
+            }
+        }
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] != j) {
+                return j;
+            }
+        }
+        return arr.length;
+    }
+
+    static void swapix(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
